4aa1e6de6b6be79bf17474bf09287589aaefd3af
Parth-26 <parthbpatel26102000@gmail.com>
2022-03-31 13:27:38 +0530

    changed a config and index file for mailing services

---
+++ Project/Helperland/config.php
@@ -0,0 +1,24 @@
+<?php 
+class Config{
+    /*------------ Database Constant ------------*/
+    const DB_SERVER = "localhost";
+    const DB_USER = "root";
+    const DB_PASS = "";
+    const DB_NAME = "helperland";
+    
+    /*------------ Users Constant--------------*/
+
+    // 1 = customer, 2 = servicer, 3 = admin
+    const USER_TYPE = [1,2,3];
+
+    const SMTP_HOST = "smtp.gmail.com";
+    const SMTP_EMAIL = "parthbpatel18@gnu.ac.in";
+    
+    //Enter password of SMTP_EMAIL to activate email functionality
+    const SMTP_PASS = "parth@26102000";
+    const SMTP_ADMIN = "parthbpatel26102000@gmail.com";
+
+    // url constant
+    const BASE_URL = "http://localhost/Tatvasoft-Project/Project/Helperland/";
+}
+?>
